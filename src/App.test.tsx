82943324ed534ad9d64202385e0b0ea59b76bf97
Oleksii Sakun <leha.sakun@gmail.com>
2021-06-15 16:58:24 +0300

    tests in progress

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("render App component", () => {
+    render(<App />);
+    expect(screen.getByText(/STATISTIC/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Search.../i)).toBeInTheDocument();
+  });
+
+  it("onChange search input", () => {
+    render(<App />);
+    expect(screen.queryByDisplayValue("qwerty")).not.toBeInTheDocument();
+    userEvent.type(screen.getByPlaceholderText("Search..."), "qwerty");
+    expect(screen.getByDisplayValue("qwerty")).toBeTruthy();
+  });
 });
